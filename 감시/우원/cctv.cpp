2c7d07cf5aadb1eeb27e1662fa047855f69accb7
woow0n <102151612+woow0n@users.noreply.github.com>
2023-03-17 08:35:37 +0900

    Update cctv.cpp

--- 감시/우원/cctv.cpp
+++ 감시/우원/cctv.cpp
@@ -1,62 +1,89 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 #define Y first
 #define X second
-
 using namespace std;
 using pic = pair<int, char>;
 using pii = pair<int, int>;
 using ll = long long;
 
+int N, M, mx = -21e8, cnt, zeroCnt;
+int Map[10][10];
+// 상 하 좌 우
+int dy[] = { 0,-1,1,0,0 };
+int dx[] = { 0,0,0,-1,1 };
+// 왼쪽으로 도는 경우 
+int turnLeft[] = {0, 3,4,2,1 };
+// 0이 주방향
 
-int N, M, cnt, y, x, dir;
-int map[55][55];
-int vst[55][55];
-
-int dy[] = {-1,0,1,0};
-int dx[] = {0,1,0,-1};
-//북0 동1 남2 서3
-int turnLeft[] = { 3,0,1,2 };
+// 여기 방향만큼 왼쪽으로 돌아야한다는 뜻;
+vector<int> cctvs[6] = {
+	{},
+	{0},
+	{0,2},
+	{0,1},
+	{0,1,2},
+	{0,1,2,3}, 
+};
+vector<pii> cctvCoors;
 
 void input() {
-	cin >> N >> M >> y >> x >> dir;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < M; x++) {
-			cin >> map[y][x];
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> Map[i][j];
+			if (Map[i][j] >= 1 && Map[i][j] <= 5) cctvCoors.push_back({ i,j });
+			if (Map[i][j] == 0) zeroCnt++;
 		}
 	}
 }
 
-int move() {
-	int cleanedCnt = 0;
-	if (map[y][x] != 2) { map[y][x] = 2; cnt++;}
-	// 네방향 확인해야해
-	for (int i = 0; i < 4; i++) {
-		//북0 동1 남2 서3
-		//int turnLeft[] = { 3,0,1,2 };
-		dir = turnLeft[dir];
-		int ny = y + dy[dir]; int nx = x + dx[dir];
-		// 0이 아니면 -> 벽이거나 청소한 곳이면 넘어가
-		if (map[ny][nx] != 0) { cleanedCnt++; continue; }
-		y = ny; x = nx; return 1;
+int turnDir(int dir, int rep) {
+	while (rep--) dir = turnLeft[dir];
+	return dir;
+}
+
+void dfs(int level) {
+	if (level == cctvCoors.size()) {
+		mx = max(mx, cnt);
+		return;
 	}
 
-	if (cleanedCnt == 4) {
-		int ny = y - dy[dir]; int nx = x - dx[dir];
-		if (map[ny][nx] == 1) return 0;
-		y = ny; x = nx; return 1;
+	auto tmpCctv = cctvCoors[level];
+	int val = Map[tmpCctv.Y][tmpCctv.X]; int end = 4;
+	if (val == 5) end = 1;
+	for (int d = 1; d <= end;d++) {
+		vector<pii> restoreCoor;
+		for (int i = 0; i < cctvs[val].size(); i++) {
+			int flag = 1;
+			int l = 1;
+			int turnRep = cctvs[val][i];
+			int realDir = turnDir(d, turnRep);
+			while (flag) {
+				int ny = tmpCctv.Y + dy[realDir] * l;
+				int nx = tmpCctv.X + dx[realDir] * l;
+				if (ny < 0 || nx < 0 || ny >= N || nx >= M) { flag = 0; continue; }
+				if ((Map[ny][nx] >= 1 && Map[ny][nx] <= 5) || Map[ny][nx] == 9) { l++; continue; }
+				if (Map[ny][nx] == 6) { flag = 0; continue; }
+				Map[ny][nx] = 9;
+				cnt++;
+				restoreCoor.push_back({ ny,nx });
+				l++;
+			}
+		}
+		dfs(level + 1);
+		for (int i = 0; i < restoreCoor.size(); i++) {
+			Map[restoreCoor[i].Y][restoreCoor[i].X] = 0;
+			cnt--;
+		}
 	}
 }
 
-
 int main(){
 	input();
-	int flag = 1;
-	while (flag) {
-		flag = move();
-	}
-	cout << cnt;
+	dfs(0);
+	cout << zeroCnt - mx;
 	return 0;
 }
