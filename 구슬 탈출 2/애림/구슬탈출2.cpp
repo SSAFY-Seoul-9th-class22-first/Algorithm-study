a6cb31a8b603693f32d3c386942d65eb1e54b917
aelim0409 <aelim0409@naver.com>
2023-04-04 16:06:10 +0900

    드디어 성공

--- 구슬 탈출 2/애림/구슬탈출2.cpp
+++ 구슬 탈출 2/애림/구슬탈출2.cpp
@@ -11,9 +11,9 @@ char map[11][11];
 pos blue, red, goal;
 
 int dy[] = { -1,0,1,0 };
-int dx[] = { 0,1,0,-1 };
+int dx[] = {  0,1,0,-1 };
 bool Rsuccess = false, Bsuccess = false;
-int res = -21e8;
+int res = 21e8;
 
 int inverse(int dir)
 {
@@ -24,94 +24,128 @@ int inverse(int dir)
 	return -1;
 }
 
-pos Rmove(int dir, pos t, pos B)
+pos Rmove(int dir, pos t)
 {
 	pos tmp = t;
 	while (1)
 	{
-		if (map[t.y][t.x] == '#')
+		if (map[tmp.y][tmp.x] == '#')
 		{
-			t.y -= dy[dir];
-			t.x -= dx[dir];
+			tmp.y -= dy[dir];
+			tmp.x -= dx[dir];
 			break;
 		}
-		if (map[t.y][t.x] == 'O')
+
+		if (map[tmp.y][tmp.x] == 'O')
 		{
 			Rsuccess = true;
 			break;
 		}
-	
-		if (t.y == B.y && t.x == B.x)
-		{
-			t.y -= dy[dir];
-			t.x -= dx[dir];
-			break;
-		}
-		t.y += dy[dir];
-		t.x += dx[dir];
 
+		tmp.y += dy[dir];
+		tmp.x += dx[dir];
 	}
-	return t;
+	return tmp;
 }
-pos Bmove(int dir, pos t,pos R)
+
+pos Bmove(int dir, pos t)
 {
 	pos tmp = t;
+
 	while (1)
 	{
-		if (map[t.y][t.x] == '#')
+		if (map[tmp.y][tmp.x] == '#')
 		{
-			t.y -= dy[dir];
-			t.x -= dx[dir];
+			tmp.y -= dy[dir];
+			tmp.x -= dx[dir];
 			break;
 		}
-		if (map[t.y][t.x] == 'O')
+		if (map[tmp.y][tmp.x] == 'O')
 		{
 			Bsuccess = true;
 			break;
 		}
-		if (t.y==R.y && t.x==R.x)
-		{
-			t.y -= dy[dir];
-			t.x -= dx[dir];
-			break;
-		}
-		t.y += dy[dir];
-		t.x += dx[dir];
 
+		tmp.y += dy[dir];
+		tmp.x += dx[dir];
 	}
-	return t;
+
+	return tmp;
 }
 
 
 void DFS(pos R, pos B, int cnt, int dir)
 {
-	if (cnt >= 10)
+	if (cnt >= res)
 		return;
+	if (cnt > 10)
+		return;
+	
 
-	if (Bsuccess == true && Rsuccess == true)
-	{
-		return;
-	}
+	Rsuccess = false;
+	Bsuccess = false;
 
-	else if (Bsuccess == true)
-		return;
-	else if (Rsuccess == true)
-	{
-		res = max(res, cnt);
-		return;
-	}
+
 	//빨간공 움직이기
-	R = Rmove(dir, R,B);
+	//pos  NR ={ R.y + dy[dir],R.x + dx[dir] };
+	pos NR=Rmove(dir, R);
 
 	//파란공 움직이기
-	B = Bmove(dir, B,R);
+	//pos NB = { B.y + dy[dir],B.x + dx[dir] };
+	pos NB= Bmove(dir, B);
 
+
+	if (Bsuccess==true)
+		return;
+	else
+	{
+		if (Rsuccess==true)
+		{
+			res = min(res, cnt);
+			return;
+		}
+	}
+
+	
+	//충돌검사
+	if (NR.y == NB.y && NR.x==NB.x)
+	{
+		if (abs(NR.y - R.y) +abs(NR.x - R.x) < abs(NB.y - B.y) +abs(NB.x - B.x))
+		{
+			NB.y -= dy[dir];
+			NB.x -= dx[dir];
+		}
+		else
+		{
+			NR.y -= dy[dir];
+			NR.x -= dx[dir];
+		}
+	}
+
+	/*
+	cout << "=================DIR "<< dir<<"====================\n";
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (i == NR.y && j == NR.x)
+				cout << 'R' << " ";
+			else if (i == NB.y && j == NB.x)
+				cout << 'B' << " ";
+			else
+				cout << map[i][j]<<" ";
+
+		}
+		cout << "\n";
+	}
+	*/
 	//0 2 // 1 3
 	for (int i = 0; i < 4; i++)
 	{
 		if (i == dir) continue;
 		if (inverse(dir) == i) continue;
-		DFS(R, B, cnt + 1, i);
+		
+		DFS(NR, NB, cnt + 1, i);
 	}
 
 }
@@ -140,7 +174,17 @@ int main()
 		}
 	}
 
-	DFS(red, blue, 1, 0);
-	cout << res;
+	for (int i = 0; i < 4; i++)
+	{
+		
+		//cout << "START DIR " << i << endl;
+		DFS(red, blue, 1, i);
+		
+	}
+
+	if (res == 21e8)
+		cout << -1;
+	else
+		cout << res;
 	return 0;
 }
