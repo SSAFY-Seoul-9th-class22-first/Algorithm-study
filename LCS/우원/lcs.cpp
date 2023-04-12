d625af7179dac0cd3639a9203275fc33d4219443
woow0n <102151612+woow0n@users.noreply.github.com>
2023-04-12 09:03:02 +0900

    Create lcs.cpp

---
+++ LCS/우원/lcs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <deque>
+
+using namespace std;
+
+int dp[1010][1010];
+int n, m;
+string a, b;
+
+int main() {
+    cin >> a >> b;
+    n = a.size();
+    m = b.size();
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    cout << dp[n][m];
+}
